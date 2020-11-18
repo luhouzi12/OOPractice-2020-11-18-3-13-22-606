85737d54545c201cd1351a6736b7398bfe7a585c
Xiaoyu Lu <xlu14@slb.com>
2020-11-18 16:50:50 +0800

    feat: pass test DriverTest

---
+++ OOPractice/Driver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPractice
+{
+    public class Driver
+    {
+        private Vehicle vehicle;
+        public Driver(Vehicle vehicle)
+        {
+            this.vehicle = vehicle;
+        }
+
+        public string Speedup()
+        {
+            return vehicle.Speedup();
+        }
+    }
+}
--- OOPracticeTest/DriverTest.cs
+++ OOPracticeTest/DriverTest.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using OOPractice;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace OOPracticeTest
 {
-    class DriverTest
+    public class DriverTest
     {
+        [Fact]
+        public void Test1()
+        {
+            var driver = new Driver(new Vehicle("Cool Car", 30));
+            //given
+            string expected = "Cool Car: speed up 30 km/h";
+
+            //when
+            string actual = driver.Speedup();
+
+            //then
+            Assert.Equal(expected, actual);
+        }
     }
 }
